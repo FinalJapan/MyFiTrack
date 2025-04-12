67a4397346c3652cf35bc124c57e9a3bb27aea04
FinalJapan <kanata1018@ezweb.ne.jp>
2025-04-13 07:12:22 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,50 @@
+const CACHE_NAME = "myfittrack-cache-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",         // または login.html に変更
+  "./login.html",
+  "./mypage.html",
+  "./login.css",
+  "./mypage.css",
+  "./auth.js",
+  "./mypage.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
 self.addEventListener("install", event => {
-    console.log("Service Worker installing.");
-    self.skipWaiting();
-  });
-  
-  self.addEventListener("activate", event => {
-    console.log("Service Worker activating.");
-  });
-  
-  self.addEventListener("fetch", event => {
-    event.respondWith(
-      fetch(event.request).catch(() => caches.match(event.request))
-    );
-  });
-  
+  console.log("[ServiceWorker] Install");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("[ServiceWorker] Pre-caching files");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  console.log("[ServiceWorker] Activate");
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            console.log("[ServiceWorker] Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
